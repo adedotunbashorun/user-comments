955872d822850c11b90894962d313ac4f2735775
Adedotun Olawale <adedotunolawale@gmail.com>
2022-06-13 22:55:23 +0100

    view user comments

--- src/controllers/CommentsController.ts
+++ src/controllers/CommentsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Controller,  Get, Put, Post, Delete } from "@overnightjs/core";
+import { Controller,  Get } from "@overnightjs/core";
 import { AbstractController } from "./AbstractController";
 import { CommentRepository as Repository } from "../repositories/CommentsRepository";
 
@@ -7,5 +7,15 @@ import { CommentRepository as Repository } from "../repositories/CommentsReposit
 export class CommentsController extends AbstractController {
   constructor() {
     super(new Repository());
+  }
+
+  @Get("/user/:userId")
+  public async userComments(req: Request, res: Response): Promise<void> {
+    try {
+      const data = await this.repository.findBy('user', req.params.userId);
+      res.status(200).send({ success: true, data });
+    } catch (error: any) {
+      res.status(401).json({ success: false, error, msg: error.message });
+    }
   }
 }
